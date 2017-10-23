ed9ad76bd25403cd393ecd03c6c08bc5df1d559f
Jd Richard H Scarberry <JdScarberry1000@gmail.com>
2017-10-23 14:55:30 -0700

    React Routing added in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
