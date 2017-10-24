bb3a50fc109cd0370eaf20a7fea8f59ba1509930
Unknown <James.Bell543@gmail.com>
2017-10-24 14:53:50 -0700

    update

--- src/components/ConnectWithUs.js
+++ src/components/ConnectWithUs.js
@@ -3,6 +3,7 @@ import JD from "../images/JD.jpg";
 import Ryan from "../images/Ryan.jpg";
 import James from "../images/James.jpg";
 import Brad from "../images/Brad.jpg";
+import fbpic from "../images/fbpic.jpg";
 
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
@@ -18,30 +19,68 @@ class ConnectWithUs extends Component {
               <br />
               <img src={Brad} />
               <br />
-              <a href="https://www.facebook.com/bradley.wong.50">Facebook</a>
+              <a href="https://www.facebook.com/bradley.wong.50">
+                <img src={fbpic} />
+              </a>
+              <br />
+              <a href="https://github.com/rfox0123">GitHub</a>
+              <br />
+              <a href="notmadeyet">Linkedin</a>
+              <br />
+              <a href="mailto:btw47@nau.edu" target="_top">
+                btw47@nau.edu
+              </a>
             </li>
             <li>
               JD Headbanger
               <br />
               <img src={JD} />
               <br />
-              <a href="https://www.facebook.com/headbangerjd">Facebook</a>
+              <a href="https://www.facebook.com/headbangerjd">
+                <img src={fbpic} />
+              </a>
+              <br />
+              <a href="https://github.com/JdScarberry1000">GitHub</a>
+              <br />
+              <a href="notmadeyet">Linkedin</a>
+              <br />
+              <a href="mailto:JdScarberry1000@gmail.com" target="_top">
+                JdScarberry1000@gmail.com
+              </a>
             </li>
             <li>
               James Bell
               <br />
               <img src={James} />
               <br />
-              <a href="https://www.facebook.com/JamesMB828">Facebook</a>
+              <a href="https://www.facebook.com/JamesMB828">
+                <img src={fbpic} />
+              </a>
               <br />
               <a href="https://github.com/MrNiceGuy1989">GitHub</a>
+              <br />
+              <a href="www.linkedin.com/in/MrNiceGuy">Linkedin</a>
+              <br />
+              <a href="mailto:James.Bell543@gmail.com" target="_top">
+                James.Bell543@gmail.com
+              </a>
             </li>
             <li>
               Ryan Fuchs
               <br />
               <img src={Ryan} />
               <br />
-              <a href="https://www.facebook.com/ryan.fuchs.50">Facebook</a>
+              <a href="https://www.facebook.com/ryan.fuchs.50">
+                <img src={fbpic} />
+              </a>
+              <br />
+              <a href="https://github.com/rfox0123">GitHub</a>
+              <br />
+              <a href="notmadeyet">Linkedin</a>
+              <br />
+              <a href="mailto:ryan.a.fuchs@gmail.com" target="_top">
+                ryan.a.fuchs@gmail.com
+              </a>
             </li>
           </h2>
         </div>
