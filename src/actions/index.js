8fd926019891297dce26297414382483ebc86d2a
Jd Richard H Scarberry <JdScarberry1000@gmail.com>
2017-11-01 13:23:55 -0700

    1: Auto stash before merge of "test1" and "test2"
    weeklyTemplate updated, still needs completed tho

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,23 @@
-import actionTypes from '../actionTypes';
-import { firebaseApp, firebaseDb } from '../server/firebase';
+import actionTypes from "../actionTypes";
+import firebase from "firebase";
+import { firebaseApp, firebaseDb } from "../server/firebase";
+import { store } from "../index.js";
 
 //ACTION CREATORS-------------------
 const updateProfile = () => {
-  window.location = '/SetProfile';
+  window.location = "/SetProfile";
 };
 
 export const loggedIn = () => {
   return {
-    type: actionTypes.LOGGED_IN,
+    type: actionTypes.LOGGED_IN
   };
 };
 
 const authError = error => {
   return {
     type: actionTypes.AUTH_ERROR,
-    payload: error,
+    payload: error
   };
 };
 
@@ -45,12 +47,19 @@ export const createUser = (email, password) => {
 };
 
 export function fetchOldStats(thisUser, time) {
+  switch (time) {
+    case "lastWeek":
+      console.log("YUS BITCH");
+    default:
+      console.log("YOU WROTE IT WRONG IDIOT");
+  }
+
   return dispatch => {
     if (thisUser != null) {
       var uid = thisUser.uid;
     }
 
-    firebaseDb.ref('users/' + uid).on('value', snapshot => {
+    firebaseDb.ref("users/" + uid).on("value", snapshot => {
       const firebaseOutput = snapshot.val();
 
       let pushList = [];
@@ -74,12 +83,11 @@ export function fetchOldStats(thisUser, time) {
       dispatch({
         type: actionTypes.FETCH_OLD_STATS,
         userID: uid,
-        fullName: firstUpload.fullName,
-        weight: firstUpload.weight,
-        ormBench: firstUpload.oneRepMax['benchORM'],
-        ormDeadlift: firstUpload.oneRepMax['deadliftORM'],
-        ormOverheadPress: firstUpload.oneRepMax['overheadPressORM'],
-        ormSquat: firstUpload.oneRepMax['squatORM'],
+        weight: lastUpload.weight,
+        ormBench: lastUpload.oneRepMax["benchORM"],
+        ormDeadlift: lastUpload.oneRepMax["deadliftORM"],
+        ormOverheadPress: lastUpload.oneRepMax["overheadPressORM"],
+        ormSquat: lastUpload.oneRepMax["squatORM"]
       });
     });
   };
@@ -91,7 +99,7 @@ export function fetchUser(thisUser) {
       var uid = thisUser.uid;
     }
 
-    firebaseDb.ref('users/' + uid).on('value', snapshot => {
+    firebaseDb.ref("users/" + uid).on("value", snapshot => {
       const firebaseOutput = snapshot.val();
 
       let pushList = [];
@@ -113,10 +121,10 @@ export function fetchUser(thisUser) {
         userID: uid,
         fullName: lastUpload.fullName,
         weight: lastUpload.weight,
-        ormBench: lastUpload.oneRepMax['benchORM'],
-        ormDeadlift: lastUpload.oneRepMax['deadliftORM'],
-        ormOverheadPress: lastUpload.oneRepMax['overheadPressORM'],
-        ormSquat: lastUpload.oneRepMax['squatORM'],
+        ormBench: lastUpload.oneRepMax["benchORM"],
+        ormDeadlift: lastUpload.oneRepMax["deadliftORM"],
+        ormOverheadPress: lastUpload.oneRepMax["overheadPressORM"],
+        ormSquat: lastUpload.oneRepMax["squatORM"]
       });
     });
   };
@@ -125,41 +133,9 @@ export function fetchUser(thisUser) {
 export const loggedOut = () => {
   return dispatch => {
     return {
-      type: actionTypes.LOGGED_OUT,
+      type: actionTypes.LOGGED_OUT
     };
   };
 };
 
-//-----Filestack-----
-export const fetchProfileImage = uid => {
-  return dispatch => {
-    firebaseDb.ref('users/' + uid).on('value', snapshot => {
-      const firebaseOutput = snapshot.val();
-
-      let pushList = [];
-      for (let prop in firebaseOutput) {
-        pushList.push(prop);
-      }
-
-      const uploadList = [];
-      for (let i = 0; i < pushList.length; i++) {
-        if (firebaseOutput[pushList[i]].profileImage) {
-          uploadList.push(firebaseOutput[pushList[i]]);
-        }
-      }
-
-      let lastUpload = uploadList[uploadList.length - 1];
-
-      if (lastUpload === undefined) {
-        dispatch({
-          type: actionTypes.NO_PROFILE_IMAGE,
-        });
-      } else {
-        dispatch({
-          type: actionTypes.PROFILE_IMAGE,
-          payload: lastUpload.profileImage,
-        });
-      }
-    });
-  };
-};
+//---------------------Jd's ACTION creator what????
