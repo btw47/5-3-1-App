29d23317b0608cb5473d6655b88fe4637c74ed14
Ryan Fuchs <ryan.a.fuchs@gmail.com>
2017-11-06 10:48:37 -0700

    calendar

--- src/components/monthCalendar.js
+++ src/components/monthCalendar.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import moment from 'moment';
 import { connect } from 'react-redux'
 import Popup from 'react-popup';
+import * as actions from '../actions';
+import firebase from 'firebase';
+import { bindActionCreators } from 'redux';
 
 
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.less';
 import BigCalendar from 'react-big-calendar';
 import BBB from './WorkoutTemplates/BBB';
+import userEvent from './eventFunction';
 import 'react-big-calendar/lib/less/styles.less';
 import './calendar.css';
 
@@ -25,43 +29,66 @@ function Event({ event }) {
 
 class Dnd extends React.Component{
   constructor(props) {
-    super(props)
-
+    super(props);
   }
 
-    render(){
-        let userEvents=[null];
-      if (this.props.state.user.selectedExercise === "boringButBig") {
-        userEvents = BBB
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        window.location = '/';
+      } else if (user) {
+        const thisUser = firebase.auth().currentUser;
+        // this.props.fetchCalendar(thisUser)
+        // console.log("SELECTED EXERCISE", this.props.state.calendar.selectedExercise)
       }
 
-        return(
-        <div>
-            <BigCalendar
-              {...this.props}
-              popup
-              selectable 
-              events = {userEvents}
-              defaultView = 'month'
-              views = {{ month: true }}
-              scrollToTime={new Date(1970, 1, 1, 6)}
-              defaultDate={new Date(2017, 9, 17)}
-              test='io'
-              onSelectEvent= {event => Popup.alert(event.desc, 'Description')}
-              components = {{
-                event: Event,
-              }}
-            />
-            <Popup
-            className="mm-popup"
-            btnClass="mm-popup__btn"
-            closeBtn={true}
-            closeHtml={null}
-            defaultOk="Ok"
-            defaultCancel="Cancel"
-            wildClasses={false}
-             />
-        </div>    
+    })
+  }
+
+
+
+  render(){
+    const {state } = this.props
+    console.log("THIS STATE", this.state)
+    console.log("THIS PROPS", this.props)
+    let userEvents = [null];
+    if (state.fetchCalendar.calendar) {
+      if (state.fetchCalendar.calendar.selectedExercise === "boringButBig") {
+        console.log("COME ON BITCH", this.state)
+        userEvents = BBB
+      } else {
+        console.log("SHIIT")
+        userEvents
+      }
+    }
+    return(
+      <div>
+        <BigCalendar
+        {...this.props}
+        popup
+        selectable
+        step= {'allday'} 
+        events = {userEvents}
+        defaultView = 'month'
+        views = {{ month: true }}
+        scrollToTime={new Date(1970, 1, 1, 6)}
+        defaultDate={new Date(2017, 9, 17)}
+        test='io'
+        onSelectEvent= {event => Popup.alert(event.desc, 'Description')}
+        components = {{
+        event: Event,
+        }}
+        />
+        <Popup
+        className="mm-popup"
+        btnClass="mm-popup__btn"
+        closeBtn={true}
+        closeHtml={null}
+        defaultOk="Ok"
+        defaultCancel="Cancel"
+        wildClasses={false}
+         />
+      </div>    
         )
     }
 }
@@ -70,5 +97,9 @@ const mapStateToProps = state => {
   return{state}
 }
 
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(actions, dispatch);
+};
 
-export default connect(mapStateToProps)(Dnd);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dnd);
