8a655d92aa5bec95fb043cf902b5f2cc48b13ff8
Unknown <James.Bell543@gmail.com>
2017-11-13 10:15:30 -0700

    Merge

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   NavLink
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import logo from '../images/weight-lifting-logo.png';
-import actionTypes from '../actionTypes';
-import SignOut from './auth/SignOut';
+import logo from "../images/weight-lifting-logo.png";
+import actionTypes from "../actionTypes";
+import SignOut from "./auth/SignOut";
 
 class NavBar extends Component {
   renderUserStatus = () => {
@@ -26,76 +26,87 @@ class NavBar extends Component {
   };
 
   style = {
-    width: '50px'
+    width: "50px"
   };
 
   render() {
     return (
-      <div className="pos-f-t navigationB">
-        <div className="collapse" id="navbarToggleExternalContent">
-          <div className="bg-dark p-4">
-            <h4 className="text-white">5-3-1 Workout App</h4>
-            <span className="text-muted">
-              <NavLink className="navbar-brand" to="/">
-                <i className="em  em-house" />
-                Home
-              </NavLink>
-              <NavLink className="navbar-brand" to="/Today">
-                <i className="em em-date" />
-                Today
-              </NavLink>
-              <NavLink className="navbar-brand" to="/Progress">
-                <i className="em em-muscle" />
-                MY GAINZ
-              </NavLink>
-              <NavLink className="navbar-brand" to="/Leaderboards">
-                <i className="em  em-trophy" />
-                Leaderboards
-              </NavLink>
-              <NavLink className="navbar-brand" to="/Forums">
-                <i className="em  em-pencil" />
-                Forums
-              </NavLink>
-              <NavLink className="navbar-brand" to="/Dashboard">
-                <i className="em  em-chart_with_upwards_trend" />
-                Dashboard
-              </NavLink>
-              <NavLink className="navbar-brand" to="/ConnectWithUs">
-                <i className="em em-sunglasses" />
-                Ladies Come Connect With Us!
-              </NavLink>
-              <NavLink className="navbar-brand" to="/LiveChat">
-                <i className="em  em-interrobang" />
-                Live Chat!
-              </NavLink>
-              <NavLink className="navbar-brand" to="/EmailSupport">
-                <i className="em  em-e-mail" />
-                Email Support
-              </NavLink>
-            </span>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button
+              type="button"
+              class="navbar-toggle"
+              data-toggle="collapse"
+              data-target="#myNavbar"
+            >
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+            </button>
+            <a className="navbar-brand" href="#" />
+            5-3-1 Work Out App
+          </div>
+
+          <div className="collapse navbar-collapse" id="myNavBar">
+            <ul className="nav navbar-nav">
+              <li>
+                <NavLink className="navbar-brand" to="/">
+                  <i className="em  em-house" />
+                  Home <span class="sr-only">(current)</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navbar-brand" to="/Today">
+                  <i className="em em-date" />
+                  Today
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navbar-brand" to="/Leaderboards">
+                  <i className="em  em-trophy" />
+                  Leaderboards
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navbar-brand" to="/Progress">
+                  <i className="em em-muscle" />
+                  MY GAINZ
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navbar-brand" to="/Forums">
+                  <i className="em  em-pencil" />
+                  Forums
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navbar-brand" to="/Dashboard">
+                  <i className="em  em-chart_with_upwards_trend" />
+                  Dashboard
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navbar-brand" to="/ConnectWithUs">
+                  <i className="em em-sunglasses" />
+                  Ladies Come Connect With Us!
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navbar-brand" to="/LiveChat">
+                  <i className="em  em-interrobang" />
+                  Live Chat!
+                </NavLink>
+              </li>
+            </ul>
+            <div
+              class="collapse navbar-collapse"
+              id="#bs-example-navbar-collapse-1"
+            />
           </div>
         </div>
-        <nav className="navbar navbar-dark bg-dark">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarToggleExternalContent"
-            aria-controls="navbarToggleExternalContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          {/* <a class="navbar-brand" href="/SignIn" className="signin">
-            Log In
-          </a> */}
-          {this.renderUserStatus()}
-
-          <img src={logo} alt="logo" style={this.style} />
-        </nav>
-      </div>
+      </nav>
     );
   }
 }
-
 export default NavBar;
